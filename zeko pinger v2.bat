b33902fbf459691e743aaa51d050b92370d6a696
ItsZeko <82521946+ItsZeko@users.noreply.github.com>
2021-04-15 12:00:59 +0200

    Add files via upload

---
+++ zeko pinger v2.bat
@@ -0,0 +1,82 @@
+@echo off
+chcp 65001
+title zeko pinger v2
+mode 60,20
+color 1
+
+echo ███████╗███████╗██╗  ██╗ ██████╗     ██████╗ ██╗███╗   ██╗ ██████╗ ███████╗██████╗     ██╗   ██╗██████╗ 
+echo ╚══███╔╝██╔════╝██║ ██╔╝██╔═══██╗    ██╔══██╗██║████╗  ██║██╔════╝ ██╔════╝██╔══██╗    ██║   ██║╚════██╗
+echo   ███╔╝ █████╗  █████╔╝ ██║   ██║    ██████╔╝██║██╔██╗ ██║██║  ███╗█████╗  ██████╔╝    ██║   ██║ █████╔╝
+echo  ███╔╝  ██╔══╝  ██╔═██╗ ██║   ██║    ██╔═══╝ ██║██║╚██╗██║██║   ██║██╔══╝  ██╔══██╗    ╚██╗ ██╔╝██╔═══╝ 
+echo ███████╗███████╗██║  ██╗╚██████╔╝    ██║     ██║██║ ╚████║╚██████╔╝███████╗██║  ██║     ╚████╔╝ ███████╗
+echo ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝      ╚═══╝  ╚══════╝
+
+set /p IP=Enter IP:
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
+                                                                                                        
+
